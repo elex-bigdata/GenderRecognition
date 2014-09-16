fb720ffe109afef6481c96f51418a858199c7647
wuzhongju <wuzhongju@126.com>
2014-09-16 13:31:17 +0800

    init

---
+++ genderRecognition/src/main/java/com/elex/userAnalyze/genderRecognition/common/PropertiesUtils.java
@@ -0,0 +1,61 @@
+package com.elex.userAnalyze.genderRecognition.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class PropertiesUtils {
+
+	private static Properties pop = new Properties();
+	static{
+		InputStream is = null;
+		try{
+			is = PropertiesUtils.class.getClassLoader().getResourceAsStream("config.properties");
+			pop.load(is);
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}finally{
+			try {
+				if(is!=null)is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+		
+	
+	
+	public static int getMergeDays(){
+		return Integer.parseInt(pop.getProperty("mergeDays"));
+	}
+	
+	public static int getTopN(){
+		return Integer.parseInt(pop.getProperty("topN"));
+	}
+	
+	public static String getRootDir(){
+		return pop.getProperty("rootdir");
+	}
+
+	public static String getHiveurl() {
+		return pop.getProperty("hive.url");
+	}
+	
+	public static String getHiveUser() {
+		return pop.getProperty("hive.user");
+	}
+
+	public static String getHiveWareHouse() {
+		return pop.getProperty("hive.warehouse");
+	}
+	
+	public static String getCfNumOfRec(){
+		return pop.getProperty("cf.numOfRec");
+	}
+	
+	public static String getCfSimilarityClassname(){
+		return pop.getProperty("cf.SimilarityClassname");
+	}
+}
---
+++ genderRecognition/src/main/java/com/elex/userAnalyze/genderRecognition/prepareWork/UserInfoUtils.java
@@ -0,0 +1,142 @@
+package com.elex.userAnalyze.genderRecognition.prepareWork;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.elex.userAnalyze.genderRecognition.common.HbaseBasis;
+
+
+public class UserInfoUtils {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws JSONException 
+	 */
+	public static void main(String[] args) throws IOException, JSONException {
+		loadUserInfoTable(args[0]);
+		loadFacebookUser(args[1]);
+		loadGenderInfo(args[3]);
+	}
+	
+	public static void loadUserInfoTable(String userInfoFile) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(userInfoFile));
+		String line = in.readLine().trim();
+		HTableInterface ud = HbaseBasis.getConn().getTable(Bytes.toBytes("337user_info"));
+		int i = 0;
+		List<Put> list = new ArrayList<Put>();
+		while (line != null){
+						
+			String[] values = line.split(",");
+			
+			if(values.length == 20){
+				i++;
+				
+				Put put = new Put(Bytes.toBytes(values[2]));				
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("name"), Bytes.toBytes(values[1]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("mail"), Bytes.toBytes(values[4]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("gender"), Bytes.toBytes(values[5]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("country"), Bytes.toBytes(values[7]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("lang"), Bytes.toBytes(values[8]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("reg"), Bytes.toBytes(values[9]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("birth"), Bytes.toBytes(values[13]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("sns"), Bytes.toBytes(values[19]));
+				
+				list.add(put);
+				
+				if(i == 1000){
+					i = 0;
+					ud.put(list);
+					list.clear();
+				}
+								
+			}
+			
+			line = in.readLine();
+			
+		}
+		ud.put(list);
+		ud.close();
+		in.close();
+	}
+	
+	public static void loadFacebookUser(String facebookUserFile) throws IOException, JSONException{
+		BufferedReader in = new BufferedReader(new FileReader(facebookUserFile));
+		String line = in.readLine().trim();
+		HTableInterface ud = HbaseBasis.getConn().getTable(Bytes.toBytes("337user_info"));
+		int i = 0;
+		List<Put> list = new ArrayList<Put>();
+		while (line != null){
+						
+			i++;
+			String id = line.substring(0, line.indexOf(" "));
+			String json = line.substring(line.indexOf("{", 1), line.length());
+			JSONObject jsonObj = new JSONObject(json);
+			String gender = jsonObj.get("gender").toString();
+			
+			Put put = new Put(Bytes.toBytes(id));				
+			put.add(Bytes.toBytes("user"), Bytes.toBytes("source"), Bytes.toBytes("facebook"));
+			put.add(Bytes.toBytes("user"), Bytes.toBytes("s_detail"), Bytes.toBytes(json));
+			put.add(Bytes.toBytes("user"), Bytes.toBytes("gender"), Bytes.toBytes(gender));			
+			list.add(put);
+			
+			if(i == 1000){
+				i = 0;
+				ud.put(list);
+				list.clear();
+			}
+			
+			line = in.readLine();
+			
+		}
+		ud.put(list);
+		ud.close();
+		in.close();
+	}
+	
+	public static void loadGenderInfo(String genderInfoFile) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(genderInfoFile));
+		String line = in.readLine().trim();
+		HTableInterface ud = HbaseBasis.getConn().getTable(Bytes.toBytes("337user_info"));
+		int i = 0;
+		List<Put> list = new ArrayList<Put>();
+		while (line != null){
+						
+			String[] values = line.split(",");
+			
+			if(values.length == 3){
+				i++;
+				
+				Put put = new Put(Bytes.toBytes(values[0]));				
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("name"), Bytes.toBytes(values[1]));
+				put.add(Bytes.toBytes("user"), Bytes.toBytes("gender"), Bytes.toBytes(values[2]));
+
+				list.add(put);
+				
+				if(i == 1000){
+					i = 0;
+					ud.put(list);
+					list.clear();
+				}
+								
+			}
+			
+			line = in.readLine();
+			
+		}
+		ud.put(list);
+		ud.close();
+		in.close();
+	}
+	
+
+}
